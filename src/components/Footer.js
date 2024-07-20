e34d40d295fa04913f7e44e9ef37b518131346a4
JSolRibeiro <jsolmidia@gmail.com>
2024-07-20 19:26:50 -0300

    correção links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,19 @@ function Footer() {
     return (
         <div className={styles.footerContainer}>
             <div className={styles.menu_bottom1}>
-                <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Brasil</a>
+                <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Brasil</a>
             </div>
             <div className={styles.menu_bottom}>
                 <div className={styles.menuBottom1}>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Sobre</a>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Publicidade</a>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Negócios</a>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Como funciona a pesquisa</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Sobre</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Publicidade</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Negócios</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Como funciona a pesquisa</a>
                 </div>
                 <div className = {styles.menuBottom2}>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Privacidade</a>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Termos</a>
-                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Configurações</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Privacidade</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Termos</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Configurações</a>
                 </div>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ function Header(){
         <div className="header">
         <div className="menu_all">
           <div className="menu">
-            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Gmail</a>
-            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Imagens</a>
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Gmail</a>
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">Imagens</a>
           </div>
           <div className="lab">
-            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">
               <img src={menu} alt="erro" />
             </a>
-            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank" rel="noreferrer">
               <img src={profile} alt="erro" />
             </a>
           </div>
