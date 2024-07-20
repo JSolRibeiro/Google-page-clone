d0017f464349d1ba1b71909ea1de1d19af6d871a
JSolRibeiro <jsolmidia@gmail.com>
2024-07-20 19:01:03 -0300

    correção atributo href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,19 @@ function Footer() {
     return (
         <div className={styles.footerContainer}>
             <div className={styles.menu_bottom1}>
-                <a href="#">Brasil</a>
+                <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Brasil</a>
             </div>
             <div className={styles.menu_bottom}>
                 <div className={styles.menuBottom1}>
-                    <a href="#">Sobre</a>
-                    <a href="#">Publicidade</a>
-                    <a href="#">Negócios</a>
-                    <a href="#">Como funciona a pesquisa</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Sobre</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Publicidade</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Negócios</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Como funciona a pesquisa</a>
                 </div>
                 <div className = {styles.menuBottom2}>
-                    <a href="#">Privacidade</a>
-                    <a href="#">Termos</a>
-                    <a href="#">Configurações</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Privacidade</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Termos</a>
+                    <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Configurações</a>
                 </div>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ function Header(){
         <div className="header">
         <div className="menu_all">
           <div className="menu">
-            <a href="#">Gmail</a>
-            <a href="#">Imagens</a>
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Gmail</a>
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">Imagens</a>
           </div>
           <div className="lab">
-            <a href="#">
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">
               <img src={menu} alt="erro" />
             </a>
-            <a href="#">
+            <a href="https://apexensino.com.br/wp-content/uploads/2019/02/iStock-1017296544-1024x683-1024x640.jpg" target="_blank">
               <img src={profile} alt="erro" />
             </a>
           </div>
